dc8fdf30508d107b138de6e64e78acc9d0202e9e
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-04-06 17:14:03 +0600

    test

--- index.js
+++ index.js
@@ -111,6 +111,4 @@ collection.find({_id: ObjectId(req.params._id)})
 
 
 
-app.listen(port||process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT||port)
